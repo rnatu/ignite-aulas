cb89c1a179c9d6c1ae0e7c433dc64d9fbcc67eb1
Renato Xavier <rnatu91@gmail.com>
2021-03-09 12:50:46 -0300

    lesson: Propriedades no React

--- reactjs/01-github-explorer/src/components/RepositoryList.jsx
+++ reactjs/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,4 +1,11 @@
-const repositoryName = 'unform2';
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+  name: 'unform',
+  description: 'Form in React',
+  link: 'https://github.com/unform/unform'
+
+}
 
 export function RepositoryList() {
   return (
@@ -6,33 +13,11 @@ export function RepositoryList() {
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <li>
-          <strong>{repositoryName}</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
-
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
-
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
       </ul>
     </section>
-  )
+  );
 }
