23321d77f3dbb2c6ebcc89053266e8eb5e019ef8
Renato Xavier <rnatu91@gmail.com>
2021-03-11 15:17:49 -0300

    install: Typescript no ReactJS

--- reactjs/01-github-explorer/webpack.config.js
+++ reactjs/01-github-explorer/webpack.config.js
@@ -6,13 +6,13 @@ const isDevelopment = process.env.NODE_ENV !== 'production'; // variavel de ambi
 module.exports = {
   mode: isDevelopment ? 'development' : 'production',
   devtool: isDevelopment ? 'eval-source-map' : 'source-map', //source maps -> eval-source-map
-  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
   devServer: { // webpack-dev-server
     contentBase: path.resolve(__dirname, 'public'),
@@ -28,7 +28,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx$/,
+        test: /\.(j|t)sx$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader', //babel-loader para códigos javascript
