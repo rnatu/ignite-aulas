08987ae614310a9de8cd48c281ad544ffd39485f
Renato Xavier <rnatu91@gmail.com>
2021-03-16 16:26:10 -0300

    lesson: criando estilos globais

---
+++ reactjs/dtmoney/src/styles/global.ts
@@ -0,0 +1,49 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: #F0F2F5;
+    --red: #E62E4D;
+    --blue: #5429CC;
+
+    --blue-light: #6933FF;
+
+    --text-title: #363F5F;
+    --text-body: #969CB3;
+
+    --shape: #FFFFFF;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  //font-size: 16px (Desktop)
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%; //16*0,9375 = 15px
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%; //16*0,87.5 = 14px
+    }
+  }
+
+  body {
+    background: var(--background);
+    //usa um antialiasing nas fontes para navegadores com a engine do chrome(webkit) para melhorar a nitidez
+    -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  //aplicar quando o elemento possuir a propriedade html disabled
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`
