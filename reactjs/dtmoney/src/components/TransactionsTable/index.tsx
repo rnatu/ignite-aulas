dd29fed04710a7cf09be6a22ad2a2a451572167a
Renato Xavier <rnatu91@gmail.com>
2021-03-19 19:02:06 -0300

    Update index.tsx

--- reactjs/dtmoney/src/components/TransactionsTable/index.tsx
+++ reactjs/dtmoney/src/components/TransactionsTable/index.tsx
@@ -36,10 +36,12 @@ export function TransactionsTable() {
             return (
               <tr key={transaction.id}>
                 <td>{transaction.title}</td>
-                <td className={transaction.type}>{new Intl.NumberFormat('pt-BR', {
-                  style: 'currency',
-                  currency: 'BRL'
-                }).format(transaction.amount)}</td>
+                <td className={transaction.type}>
+                  {new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                  }).format(transaction.amount)}
+                </td>
                 <td>{transaction.category}</td>
                 <td>
                   {new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))}
