829084e791b0dffafce4db9e058a1826679e4218
Renato Xavier <rnatu91@gmail.com>
2021-03-19 17:33:44 -0300

    feat: inserindo transação na API

--- reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
+++ reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -3,8 +3,10 @@ import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
-import { Container, RadioBox, TransactionTypeContainer } from './styles';
 
+import { api } from '../../services/api';
+
+import { Container, RadioBox, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -20,7 +22,14 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    console.log({title, value, category, type})
+    const data = {
+      title,
+      value,
+      category,
+      type
+    };
+
+    api.post('/transactions', data);
   }
 
   return (
