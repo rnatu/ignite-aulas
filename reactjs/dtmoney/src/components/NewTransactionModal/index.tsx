3cee68cf1163daa0e834b9ee331dd810e6853037
Renato Xavier <rnatu91@gmail.com>
2021-03-19 14:32:56 -0300

    feat: criando os botões de tipo

--- reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
+++ reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, TransactionTypeContainer } from './styles';
 
 
 interface NewTransactionModalProps {
@@ -36,6 +38,22 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
           type="number"
           placeholder="Valor"
         />
+
+        <TransactionTypeContainer>
+          <button
+            type="button"
+          >
+            <img src={incomeImg} alt="Entrada"/>
+            <span>Entrada</span>
+          </button>
+
+          <button
+            type="button"
+          >
+            <img src={outcomeImg} alt="Saída"/>
+            <span>Entrada</span>
+          </button>
+        </TransactionTypeContainer>
 
         <input
           placeholder="Categoria"
