e0805801268660db12974eee10b0308af9126b7b
Renato Xavier <rnatu91@gmail.com>
2021-03-21 15:16:03 -0300

    feat: calculando resumo

--- reactjs/dtmoney/src/components/Summary/index.tsx
+++ reactjs/dtmoney/src/components/Summary/index.tsx
@@ -9,6 +9,22 @@ import { Container } from "./style";
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
 
+  const summary = transactions.reduce((acc, transaction) => {
+    if(transaction.type === 'deposit') {
+      acc.deposits += transaction.amount;
+      acc.total += transaction.amount;
+    } else {
+      acc.withdraws += transaction.amount;
+      acc.total -= transaction.amount;
+    }
+
+    return acc;
+  }, {
+    deposits: 0,
+    withdraws: 0,
+    total: 0
+  })
+
   console.log(transactions)
 
   return (
@@ -18,21 +34,36 @@ export function Summary() {
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas"/>
         </header>
-        <strong>R$1000,00</strong>
+          <strong>
+            {new Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL'
+            }).format(summary.deposits)}
+          </strong>
       </div>
       <div>
         <header>
           <p>Saídas</p>
           <img src={outcomeImg} alt="Entradas"/>
         </header>
-        <strong>- R$500,00</strong>
+        <strong>
+          -{new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.withdraws)}
+        </strong>
       </div>
       <div className="highlight-background">
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Entradas"/>
         </header>
-        <strong>R$500,00</strong>
+          <strong>
+            {new Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL'
+            }).format(summary.total)}
+          </strong>
       </div>
     </Container>
   )
