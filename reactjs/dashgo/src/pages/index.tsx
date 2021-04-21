44be4178e07bcecb1644725b74ea2a1bb298fe4d
Renato Xavier <rnatu91@gmail.com>
2021-04-21 13:06:39 -0300

    feat: add page: sign in

--- reactjs/dashgo/src/pages/index.tsx
+++ reactjs/dashgo/src/pages/index.tsx
@@ -1,5 +1,64 @@
+import { Flex, Input, Button, Stack, FormLabel, FormControl } from "@chakra-ui/react";
+
 export default function Home() {
   return (
-      <h1>Dashgo</h1>
-  )
+    <Flex
+      w="100vw" //width
+      h="100vh" //height
+      align="center" //align-center
+      justify="center" //justify-center
+    >
+      <Flex
+        as="form"
+        w="100%" //width
+        maxWidth={360} //360px
+        bg="gray.800"
+        p="8" //padding 8(medida do chakra ui)
+        borderRadius={8} //8px
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              name="email"
+              id="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900",
+              }}
+              size="lg"
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+            <Input
+              name="password"
+              id="password"
+              type="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900",
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+        <Button
+          type="submit"
+          mt="6" //margin-top 6(medida do chakra ui)
+          colorScheme="pink"
+          size="lg"
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
