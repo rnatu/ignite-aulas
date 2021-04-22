cc90eec57f3784ffa0df34df41eaea954f6dff89
Renato Xavier <rnatu91@gmail.com>
2021-04-22 14:17:19 -0300

    feat: input

---
+++ reactjs/dashgo/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import { Flex } from '@chakra-ui/react';
+
+export function Header() {
+  return (
+    <Flex
+      w="100%"
+      h="20" //medida chakra UI
+      as="header"
+      maxW={1480}
+    >
+
+    </Flex>
+  );
+}
