8be239985a18c03c3fb312a4b30129e8b15591ba
Renato Xavier <rnatu91@gmail.com>
2021-04-23 16:01:33 -0300

    feat: responsive header

--- reactjs/dashgo/src/components/Header/Profile.tsx
+++ reactjs/dashgo/src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return(
     <Flex align="center">
-    <Box mr="4" textAlign="right">
-      <Text>Renato Xavier</Text>
-      <Text color="gray.300" fontSize="small">
-        rnatu91@gmail.com
-      </Text>
-    </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+         <Text>Renato Xavier</Text>
+         <Text color="gray.300" fontSize="small">
+           rnatu91@gmail.com
+         </Text>
+       </Box>
+      )}
 
     <Avatar size="md" name="Renato Xavier" src="https://github.com/rnatu.png"></Avatar>
   </Flex>
--- reactjs/dashgo/src/components/Header/index.tsx
+++ reactjs/dashgo/src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, useBreakpointValue } from '@chakra-ui/react';
 
 import { Logo } from './Logo';
 import { NotificationsNav } from './NotificationsNav';
@@ -6,23 +6,27 @@ import { Profile } from './Profile';
 import { SearchBox } from './SeachBox';
 
 export function Header() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  })
   return (
     <Flex
-      w="100%"
-      h="20" //medida chakra UI
       as="header"
+      w="100%"
       maxW={1480}
+      h="20" //medida chakra UI
       mx="auto"
       mt="4"
       px="6"
       align="center"
     >
       <Logo />
-      <SearchBox />
+      {isWideVersion && <SearchBox />}
 
       <Flex align="center" ml="auto">
         <NotificationsNav />
-        <Profile />
+        <Profile showProfileData={isWideVersion}/>
       </Flex>
     </Flex>
   );
