d58ff66f3db4db390d2aee9314620f56440b76bc
Renato Xavier <rnatu91@gmail.com>
2021-04-23 10:10:58 -0300

    feat: create page users

--- reactjs/dashgo/src/pages/dashboard.tsx
+++ reactjs/dashgo/src/pages/dashboard.tsx
@@ -69,18 +69,16 @@ export default function Dashboard() {
       <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
         <SideBar />
 
-      <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
-        <Box p="8" bg="gray.800" borderRadius={8} pb="4">
-          <Text fontSize="lg" mb="4">Inscritos da semana</Text>
-          <Chart options={options} series={series} type="area" height={160}/>
-        </Box>
-        <Box p="8" bg="gray.800" borderRadius={8} pb="4">
-          <Text fontSize="lg" mb="4">Taxa de abertura</Text>
-          <Chart options={options} series={series} type="area" height={160}/>
-
-        </Box>
-
-      </SimpleGrid>
+        <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
+          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+            <Text fontSize="lg" mb="4">Inscritos da semana</Text>
+            <Chart options={options} series={series} type="area" height={160}/>
+          </Box>
+          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+            <Text fontSize="lg" mb="4">Taxa de abertura</Text>
+            <Chart options={options} series={series} type="area" height={160}/>
+          </Box>
+        </SimpleGrid>
       </Flex>
     </Flex>
   )
