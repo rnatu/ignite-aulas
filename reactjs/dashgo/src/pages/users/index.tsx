12243f3b4f32d660cc88367c06fbd495e1a04889
Renato Xavier <rnatu91@gmail.com>
2021-04-23 10:19:43 -0300

    feat: create Pagination component

--- reactjs/dashgo/src/pages/users/index.tsx
+++ reactjs/dashgo/src/pages/users/index.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th,
 import React from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 import { SideBar } from "../../components/SideBar";
 
 export default function UserList() {
@@ -120,8 +121,9 @@ export default function UserList() {
               </Tr>
             </Tbody>
           </Table>
-        </Box>
 
+          <Pagination />
+        </Box>
       </Flex>
     </Box>
   );
