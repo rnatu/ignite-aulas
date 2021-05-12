f3cf85c57193cd5d660230e3a5982875b5556eba
Renato Xavier <rnatu91@gmail.com>
2021-05-12 12:27:46 -0300

    add: lib faker on miragejs

--- reactjs/dashgo/src/pages/users/index.tsx
+++ reactjs/dashgo/src/pages/users/index.tsx
@@ -1,6 +1,6 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
@@ -11,6 +11,12 @@ export default function UserList() {
     base: false,
     lg: true
   });
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users')
+    .then(response => response.json())
+    .then(data => console.log(data));
+  }, [])
 
   return(
     <Box>
--- reactjs/dashgo/src/services/mirage/index.ts
+++ reactjs/dashgo/src/services/mirage/index.ts
@@ -1,36 +1,56 @@
-import { createServer, Model } from 'miragejs';
+import { createServer, Factory, Model } from "miragejs";
+import faker from 'faker';
 
 type User = {
   name: string;
   email: string;
   created_at: string;
-}
+};
 
 export function makeServer() {
   const server = createServer({
-
     //tipos de dados que serão armazenados no banco de dados "fictício"
     models: {
-      user: Model.extend<Partial<User>>({})
+      user: Model.extend<Partial<User>>({}),
+    },
+
+    //geração de dados em massa
+    factories: {
+      user: Factory.extend({
+        name(i: number){
+          return `User ${i + 1}`
+        },
+        email() {
+          return faker.internet.email().toLowerCase();
+        },
+        //pode ser em camelCase pois o mirage ira converter para snake_case
+        createdAt() {
+          return faker.date.recent(10);
+        },
+      })
+    },
+    //geração de dados iniciais
+    seeds(server) {
+      server.createList('user', 200)
     },
 
     routes() {
       // setando o namespace para 'api' ex: localhost:3000/api/users
-      this.namespace = 'api';
+      this.namespace = "api";
 
       // adicionando delay nas chamadas api no mirage
       this.timing = 750;
 
       //Shorthands - automatizando as rotas
-      this.get('/users');
-      this.post('/users');
+      this.get("/users");
+      this.post("/users");
 
       // resetando o namespace após utilização para não conflitar com a api routes do next
-      this.namespace = '';
+      this.namespace = "";
       // passando adiante chamadas api que não forem do mirage, prosseguindo para as api routes
-      this.passthrough()
-    }
-  })
+      this.passthrough();
+    },
+  });
 
   return server;
 }
