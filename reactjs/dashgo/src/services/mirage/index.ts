7dfd2d614dddbebabe6a4165fb37d71862a36824
Renato Xavier <rnatu91@gmail.com>
2021-05-14 12:46:15 -0300

    feat: lógica de paginação

--- reactjs/dashgo/src/services/mirage/index.ts
+++ reactjs/dashgo/src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model } from "miragejs";
+import { createServer, Factory, Model, Response } from "miragejs";
 import faker from 'faker';
 
 type User = {
@@ -32,10 +32,11 @@ export function makeServer() {
     },
     //geração de dados iniciais
     seeds(server) {
-      server.createList('user', 10)
+      server.createList('user', 200)
     },
 
     routes() {
+
       // setando o namespace para 'api' ex: localhost:3000/api/users
       this.namespace = "api";
 
@@ -43,7 +44,26 @@ export function makeServer() {
       this.timing = 750;
 
       //Shorthands - automatizando as rotas
-      this.get("/users");
+      this.get("/users", function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams
+
+        // total de registros por model
+        const total = schema.all('user').length
+
+        //converter para number, pois todo queryParams vem como string
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = this.serialize(schema.all('user')).users.slice(pageStart, pageEnd);
+
+        return new Response(
+          200,
+          { 'x-total-count': String(total) },
+          { users }
+        )
+
+      });
+
       this.post("/users");
 
       // resetando o namespace após utilização para não conflitar com a api routes do next
@@ -51,6 +71,7 @@ export function makeServer() {
       // passando adiante chamadas api que não forem do mirage, prosseguindo para as api routes
       this.passthrough();
     },
+
   });
 
   return server;
