602fe225114f7abc45201104907996d21ad116a6
Renato Xavier <rnatu91@gmail.com>
2021-05-21 16:22:15 -0300

    feat: cookies storage

--- reactjs/autenticacao-e-autorizacao/ignite-reactjs-next-auth-jwt-main/contexts/AuthContext.tsx
+++ reactjs/autenticacao-e-autorizacao/ignite-reactjs-next-auth-jwt-main/contexts/AuthContext.tsx
@@ -1,45 +1,74 @@
-import { error } from 'console'
-import { createContext, ReactNode  } from 'react'
-import { api } from '../services/api'
+import { createContext, ReactNode, useState } from "react";
+import Router from 'next/router';
+import { api } from "../services/api";
+import { setCookie } from 'nookies'
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
 
 type SignInCredentials = {
   email: string;
   password: string;
-}
+};
 
 type AuthContextData = {
   isAuthenticated: boolean;
   signIn: (credentials: SignInCredentials) => Promise<void>;
-}
+  user: User;
+};
 
 type AuthProviderProps = {
   children: ReactNode; //componentes, textos, números...
-}
+};
 
 //Context
-export const AuthContext = createContext({} as AuthContextData)
+export const AuthContext = createContext({} as AuthContextData);
 
 //Provider Context
-export function AuthProvider({children}: AuthProviderProps) {
-  const isAuthenticated = false;
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>();
+  const isAuthenticated = !!user;
 
-  async function signIn({email, password}) {
+  async function signIn({ email, password }) {
     try {
-      const response = await api.post('sessions', {
+      const response = await api.post("sessions", {
         email,
-        password
-      })
-      console.log(response.data)
-    } catch (err) {
-      console.log(err)
-      
-    }
+        password,
+      });
 
+      const { token, refreshToken, permissions, roles } = response.data;
+
+      //undefined, pois está sendo setado pelo lado do browser
+      setCookie(undefined, 'nextauth.token', token, {
+        //por quanto tempo manter o cookie salvo no navegador, porem a renovação ou controle mesmo antes de expirar é feita pelo back-end
+        maxAge: 60 * 60 * 24 * 30, //30 days
+        // quais caminhos da aplicação vão ter acesso a esse cookie, colocando '/', voce informa que todos os endereços vao ter acesso. Uma forma de utilizar o cookie globalmente na aplicação
+        path: '/'
+      })
+
+      setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, //30 days
+        path: '/'
+      })
+
+      setUser({
+        email,
+        permissions,
+        roles,
+      });
+
+      Router.push('/dashboard');
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   return (
-    <AuthContext.Provider value={{isAuthenticated, signIn}}>
+    <AuthContext.Provider value={{ isAuthenticated, signIn, user }}>
       {children}
     </AuthContext.Provider>
-  )
+  );
 }
