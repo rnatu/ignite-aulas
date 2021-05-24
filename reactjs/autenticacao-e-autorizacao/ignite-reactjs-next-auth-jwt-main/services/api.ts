c5c10ce7828b5693d2ec23bf511c2abb3074c21b
Renato Xavier <rnatu91@gmail.com>
2021-05-24 15:59:53 -0300

    feat: requisition queue on axios

--- reactjs/autenticacao-e-autorizacao/ignite-reactjs-next-auth-jwt-main/services/api.ts
+++ reactjs/autenticacao-e-autorizacao/ignite-reactjs-next-auth-jwt-main/services/api.ts
@@ -2,6 +2,8 @@ import axios, { AxiosError } from "axios";
 import { parseCookies, setCookie } from "nookies";
 
 let cookies = parseCookies();
+let isRefreshing = false;
+let failedRequestQueue = [];
 
 export const api = axios.create({
   baseURL: "http://localhost:3333",
@@ -20,31 +22,62 @@ api.interceptors.response.use(
         //renovar o token
         cookies = parseCookies();
         const refreshToken = cookies["nextauth.refreshToken"];
+        const originalConfig = error.config;
 
-        api.post("/refresh", {
-            refreshToken,
-        }).then((response) => {
-            const { token } = response.data;
-            //undefined, pois está sendo setado pelo lado do browser
-            setCookie(undefined, "nextauth.token", token, {
-              //por quanto tempo manter o cookie salvo no navegador, porem a renovação ou controle mesmo antes de expirar é feita pelo back-end
-              maxAge: 60 * 60 * 24 * 30, //30 days
-              // quais caminhos da aplicação vão ter acesso a esse cookie, colocando '/', voce informa que todos os endereços vao ter acesso. Uma forma de utilizar o cookie globalmente na aplicação
-              path: "/",
-            });
+        if (!isRefreshing) {
+          isRefreshing = true;
 
-            setCookie(
-              undefined,
-              "nextauth.refreshToken",
-              response.data.refreshToken,
-              {
+          api
+            .post("/refresh", {
+              refreshToken,
+            })
+            .then((response) => {
+              const { token } = response.data;
+              //undefined, pois está sendo setado pelo lado do browser
+              setCookie(undefined, "nextauth.token", token, {
+                //por quanto tempo manter o cookie salvo no navegador, porem a renovação ou controle mesmo antes de expirar é feita pelo back-end
                 maxAge: 60 * 60 * 24 * 30, //30 days
+                // quais caminhos da aplicação vão ter acesso a esse cookie, colocando '/', voce informa que todos os endereços vao ter acesso. Uma forma de utilizar o cookie globalmente na aplicação
                 path: "/",
-              }
-            );
-            
-            api.defaults.headers['Authorization'] = `Bearer ${token}`
+              });
+
+              setCookie(
+                undefined,
+                "nextauth.refreshToken",
+                response.data.refreshToken,
+                {
+                  maxAge: 60 * 60 * 24 * 30, //30 days
+                  path: "/",
+                }
+              );
+
+              api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+              failedRequestQueue.forEach((request) => request.onSuccess(token));
+              failedRequestQueue = [];
+            })
+            .catch((err) => {
+              failedRequestQueue.forEach((request) => request.onFailure(err));
+              failedRequestQueue = [];
+            })
+            .finally(() => {
+              isRefreshing = false;
+            });
+          }
+          
+          console.log('ok')
+        return new Promise((resolve, reject) => {
+          failedRequestQueue.push({
+            onSuccess: (token: string) => {
+              originalConfig.headers["Authorization"] = `Barrer ${token}`;
+
+              resolve(api(originalConfig));
+            },
+            onFailure: (err: AxiosError) => {
+              reject(err);
+            },
           });
+        });
       } else {
         //deslogar o usuário
       }
