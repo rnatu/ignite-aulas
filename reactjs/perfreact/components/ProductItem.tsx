58fbf39b1a454ea120442d5619e79a006c438a78
Renato Xavier <rnatu91@gmail.com>
2021-06-14 15:52:35 -0300

    feat: add code splitting / lazy loading

--- reactjs/perfreact/components/ProductItem.tsx
+++ reactjs/perfreact/components/ProductItem.tsx
@@ -1,4 +1,14 @@
-import { memo } from 'react';
+import { memo, useState } from 'react';
+
+import dynamic from 'next/dynamic';
+
+import { AddProductToWishlistProps } from './AddProductToWishlist'
+
+const AddProductToWishlist = dynamic<AddProductToWishlistProps>(() => {
+  return import('./AddProductToWishlist').then(mod => mod.AddProductToWishlist)
+}, {
+  loading: () => <span>Carregando...</span>
+})
 
 interface ProductItemProps {
   product: {
@@ -11,10 +21,20 @@ interface ProductItemProps {
 }
 
 export function ProductItemComponent({ product, onAddToWishlist }: ProductItemProps) {
+  const [isAddingToWishlist, setIsAddingToWishlist] = useState(false);
+
   return (
     <div>
       {product.title} - <strong>{product.priceFormatted}</strong>
-      <button onClick={() => onAddToWishlist(product.id)}>Add to wishlist</button>
+
+      <button onClick={() => setIsAddingToWishlist(true)}>Adicionar aos favoritos</button>
+
+      {isAddingToWishlist && (
+        <AddProductToWishlist 
+        onAddToWishlist={() => onAddToWishlist(product.id)}
+        onRequestClose={() => setIsAddingToWishlist(false)}
+      />
+      )}
     </div>
   )
 }
