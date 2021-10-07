68ec5acfb3f623216a38c8dfa170d9b18f5b9e1e
Renato Xavier <rnatu91@gmail.com>
2021-10-07 16:14:36 -0300

    feat: add fake api with json server

--- reactjs/redux/src/components/Catalog.tsx
+++ reactjs/redux/src/components/Catalog.tsx
@@ -1,9 +1,26 @@
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import api from "../services/api";
+import { IProduct } from "../store/modules/cart/types";
 
 export function Catalog() {
-  const catalog = useSelector(state => state);
+  const [catalog, setCatalog] = useState<IProduct[]>([]);
 
-  console.log(catalog)
+  useEffect(() => {
+    api.get("products").then((response) => {
+      setCatalog(response.data);
+    });
+  }, []);
 
-  return <h1>Catalog</h1>;
+  return (
+    <>
+      <h1>Catalog</h1>
+      {catalog.map((product) => (
+        <article key={product.id}>
+          <strong>{product.title}</strong> {" - "}
+          <span>{product.price}</span> {"  "}
+          <button type="button">Comprar</button>
+        </article>
+      ))}
+    </>
+  );
 }
