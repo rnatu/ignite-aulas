494e7a40b5949bfade4c3a1ebd2de904b4e64fa8
Renato Xavier <rnatu91@gmail.com>
2021-10-11 15:55:55 -0300

    feat: changing states

---
+++ reactjs/redux/src/components/Cart.tsx
@@ -0,0 +1,25 @@
+import { useSelector } from "react-redux";
+
+export function Cart() {
+  const state = useSelector(state => state);
+
+  console.log(state);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Produto</th>
+          <th>Preço</th>
+          <th>Quantidade</th>
+          <th>Subtotal</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
