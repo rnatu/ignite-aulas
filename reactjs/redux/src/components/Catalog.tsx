b2b62a44f18a4e100faba1b9c15d94159fa98fa2
Renato Xavier <rnatu91@gmail.com>
2021-10-13 16:09:44 -0300

    feat: add catalog component

--- reactjs/redux/src/components/Catalog.tsx
+++ reactjs/redux/src/components/Catalog.tsx
@@ -1,11 +1,11 @@
-import { useCallback, useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+
 import api from "../services/api";
-import { addProductToCart } from "../store/modules/cart/actions";
+
 import { IProduct } from "../store/modules/cart/types";
+import { CatalogItem } from "./CatalogItem";
 
 export function Catalog() {
-  const dispatch = useDispatch();
   const [catalog, setCatalog] = useState<IProduct[]>([]);
 
   useEffect(() => {
@@ -14,19 +14,11 @@ export function Catalog() {
     });
   }, []);
 
-  const handleAddProductToCart = useCallback((product: IProduct) => {
-    dispatch(addProductToCart(product))
-  }, []);
-
   return (
     <>
       <h1>Catalog</h1>
       {catalog.map((product) => (
-        <article key={product.id}>
-          <strong>{product.title}</strong> {" - "}
-          <span>{product.price}</span> {"  "}
-          <button type="button" onClick={() => handleAddProductToCart(product)}>Comprar</button>
-        </article>
+        <CatalogItem key={product.id} product={product} />
       ))}
     </>
   );
