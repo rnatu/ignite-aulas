2650a800e487e8a2d3724c06571fcf57661a72ca
Renato Xavier <rnatu91@gmail.com>
2021-10-21 18:39:11 -0300

    feat: finished stock check process

--- reactjs/redux/src/store/modules/cart/sagas.ts
+++ reactjs/redux/src/store/modules/cart/sagas.ts
@@ -1,18 +1,51 @@
-import { all, takeLatest, select } from "redux-saga/effects";
-import { addProductToCartRequest } from "./actions";
+import { all, takeLatest, select, call, put } from "redux-saga/effects";
+import {
+  addProductToCartFailure,
+  addProductToCartRequest,
+  addProductToCartSuccess,
+} from "./actions";
 import { IState } from "../../index";
+import { AxiosResponse } from "axios";
+import api from "../../../services/api";
 
 type CheckProductStockRequest = ReturnType<typeof addProductToCartRequest>;
 
+interface IStockResponse {
+  id: number;
+  quantity: number;
+}
+
+//o saga necessita de funções geradoras para trabalhar com os métodos do redux-saga/effects
+//todos os métodos do redux-saga/effects que forem ser utilizados dentro da função geradora vão necessitar do yield antes
 function* checkProductStock({ payload }: CheckProductStockRequest) {
   const { product } = payload;
 
+  //buscando informações do estado e fazendo um find
+  //funciona como um useSelector, só que no saga
   const currentQuantity: number = yield select((state: IState) => {
-    return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0;
+    return (
+      state.cart.items.find((item) => item.product.id === product.id)
+        ?.quantity ?? 0
+    );
   });
 
-  console.log(currentQuantity)
+  //get com axios no saga
+  const availableStockResponse: AxiosResponse<IStockResponse> = yield call(
+    api.get,
+    `stock/${product.id}`
+  );
+
+  if (availableStockResponse.data.quantity > currentQuantity) {
+    //put funciona como o dispatch, só que no saga
+    yield put(addProductToCartSuccess(product));
+  } else {
+    yield put(addProductToCartFailure(product.id));
+  }
+
+  console.log(currentQuantity);
 }
 
 // primeiro parametro é a action, e o segundo a função a ser executada como middleware
-export default all([takeLatest("ADD_PRODUCT_TO_CART_REQUEST", checkProductStock)]);
+export default all([
+  takeLatest("ADD_PRODUCT_TO_CART_REQUEST", checkProductStock),
+]);
