7e602e90d413c540e55e46ec28680ead80b8cb72
Renato Xavier <rnatu91@gmail.com>
2021-10-21 18:09:40 -0300

    feat: add saga function to  getting quantity from cart

--- reactjs/redux/src/store/modules/cart/sagas.ts
+++ reactjs/redux/src/store/modules/cart/sagas.ts
@@ -1,7 +1,18 @@
-import { all, takeLatest } from "redux-saga/effects";
+import { all, takeLatest, select } from "redux-saga/effects";
+import { addProductToCart } from "./actions";
+import { IState } from "../../index";
 
-function checkProductStock() {
-  console.log("Adicionou ao carrinho");
+type CheckProductStockRequest = ReturnType<typeof addProductToCart>;
+
+function* checkProductStock({ payload }: CheckProductStockRequest) {
+  const { product } = payload;
+
+  const currentQuantity: number = yield select((state: IState) => {
+    return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0;
+  });
+
+  console.log(currentQuantity)
 }
 
+// primeiro parametro é a action, e o segundo a função a ser executada como middleware
 export default all([takeLatest("ADD_PRODUCT_TO_CART", checkProductStock)]);
