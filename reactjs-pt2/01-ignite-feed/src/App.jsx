8ca2b3257e79c94a94e21dc65a4236e13a11c099
Renato <rnatu91@gmail.com>
2022-06-27 19:54:29 -0300

    style: initial css

--- reactjs-pt2/01-ignite-feed/src/App.jsx
+++ reactjs-pt2/01-ignite-feed/src/App.jsx
@@ -1,5 +1,13 @@
+import { Header } from "./components/Header";
+
+import "./global.css";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
