7dd64ca697410c7bf176574f5b3b87499fd68d0e
Renato <rnatu91@gmail.com>
2022-06-28 20:09:37 -0300

    feat: add Comment component html structure

---
+++ reactjs-pt2/01-ignite-feed/src/components/Comment.jsx
@@ -0,0 +1,36 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/rnatu.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Renato Xavier</strong>
+              <time title="11 de maio às 08:13" dateTime="2022-05-11 08:13:30">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
--- reactjs-pt2/01-ignite-feed/src/components/Post.jsx
+++ reactjs-pt2/01-ignite-feed/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
 export function Post() {
@@ -34,6 +35,21 @@ export function Post() {
           <a href="">#nlw</a>
           <a href="">#rocketseat</a>
         </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Escreva seu comentário..." />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
       </div>
     </article>
   );
