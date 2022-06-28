0f07af7fddf7c554b7d3249dfc86704776869a62
Renato <rnatu91@gmail.com>
2022-06-28 14:12:37 -0300

    feat: add Post html structure

--- reactjs-pt2/01-ignite-feed/src/App.jsx
+++ reactjs-pt2/01-ignite-feed/src/App.jsx
@@ -1,9 +1,9 @@
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
 
 import "./global.css";
 import styles from "./App.module.css";
-import { Post } from "./components/Post";
 
 function App() {
   return (
