65800fbbddce1bb4497d1815579d68c81e1efb9d
Renato <rnatu91@gmail.com>
2022-08-04 13:50:26 -0300

    feat: add GlobalStyle with styled components

--- reactjs-pt2/02-ignite-timer/src/App.tsx
+++ reactjs-pt2/02-ignite-timer/src/App.tsx
@@ -1,11 +1,14 @@
 import { ThemeProvider } from "styled-components";
 
 import { Button } from "./components/Button";
+import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button variant="danger" />
