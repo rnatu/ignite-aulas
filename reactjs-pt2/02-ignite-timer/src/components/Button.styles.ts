824b07078e0404be29b35813d3e052a8ac3c214e
Renato <rnatu91@gmail.com>
2022-08-05 13:50:27 -0300

    feat: add esLint on project

--- reactjs-pt2/02-ignite-timer/src/components/Button.styles.ts
+++ reactjs-pt2/02-ignite-timer/src/components/Button.styles.ts
@@ -1,28 +1,26 @@
-import styled, { css } from "styled-components";
+import styled, { css } from 'styled-components';
 
-export type ButtonVariant = "primary" | "secondary" | "danger" | "success";
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
 
 interface ButtonContainerProps {
   variant: ButtonVariant;
 }
 
 const buttonVariants = {
-  primary: "purple",
-  secondary: "orange",
-  danger: "red",
-  success: "green",
+  primary: 'purple',
+  secondary: 'orange',
+  danger: 'red',
+  success: 'green',
 };
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   height: 50px;
   width: 100px;
 
-  background-color: ${(props) => props.theme["green-500"]};
+  background-color: ${(props) => props.theme['green-500']};
   color: ${(props) => props.theme.white};
 
-  /* ${(props) => {
-    return css`
+  /* ${(props) => css`
       background-color: ${buttonVariants[props.variant]};
-    `;
-  }} */
+    `} */
 `;
--- reactjs-pt2/02-ignite-timer/src/styles/global.ts
+++ reactjs-pt2/02-ignite-timer/src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   * {
@@ -8,8 +8,8 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    background-color: ${(props) => props.theme["gray-900"]};
-    color: ${(props) => props.theme["gray-300"]};
+    background-color: ${(props) => props.theme['gray-900']};
+    color: ${(props) => props.theme['gray-300']};
   }
 
   body, input, textarea, button{
@@ -21,7 +21,7 @@ export const GlobalStyle = createGlobalStyle`
   :focus {
     outline: 0;
     
-    box-shadow: 0 0 0 2px ${(props) => props.theme["green-500"]};
+    box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
   }
 
 `;
