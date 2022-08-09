dca65cefc1e964ab2df9970aac16f0edf3fcc1f2
Renato <rnatu91@gmail.com>
2022-08-09 17:12:49 -0300

    feat: add layout routes

--- reactjs-pt2/02-ignite-timer/src/Router.tsx
+++ reactjs-pt2/02-ignite-timer/src/Router.tsx
@@ -1,12 +1,17 @@
+/* eslint-disable react/jsx-no-undef */
+/* eslint-disable react/jsx-one-expression-per-line */
 import { Routes, Route } from 'react-router-dom';
+import { DefaultLayout } from './layouts/DefaultLayout';
 import { History } from './pages/History';
 import { Home } from './pages/Home';
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   );
 }
