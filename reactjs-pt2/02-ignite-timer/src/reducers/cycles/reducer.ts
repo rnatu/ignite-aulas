d4ef5187cc158aded8b8f9558fff2b3d56cfe559
Renato Xavier <rnatu91@gmail.com>
2023-01-29 17:40:36 -0300

    feat: add produce from immer on reducer ADD_NEW_CYCLE case

--- reactjs-pt2/02-ignite-timer/src/reducers/cycles/reducer.ts
+++ reactjs-pt2/02-ignite-timer/src/reducers/cycles/reducer.ts
@@ -20,11 +20,17 @@ interface CyclesState {
 export function cyclesReducer(state: CyclesState, action: any) {
   switch (action.type) {
     case ActionTypes.ADD_NEW_CYCLE:
-      return {
-        ...state,
-        cycles: [...state.cycles, action.payload.newCycle],
-        activeCycleId: action.payload.newCycle.id,
-      };
+      // % Sem immer
+      // return {
+      //   ...state,
+      //   cycles: [...state.cycles, action.payload.newCycle],
+      //   activeCycleId: action.payload.newCycle.id,
+      // };
+      // % Com immer
+      return produce(state, (draft) => {
+        draft.cycles.push(action.payload.newCycle);
+        draft.activeCycleId = action.payload.newCycle.id;
+      });
 
     case ActionTypes.INTERRUPT_CURRENT_CYCLE:
       return {
