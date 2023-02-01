44f00268e1a7341cd4047a5893134ae45ec868c2
Renato Xavier <rnatu91@gmail.com>
2023-02-01 16:07:05 -0300

    fix: adjusted the display of time numbers on the Countdown component

--- reactjs-pt2/02-ignite-timer/src/contexts/CyclesContext.tsx
+++ reactjs-pt2/02-ignite-timer/src/contexts/CyclesContext.tsx
@@ -1,3 +1,4 @@
+import { differenceInSeconds } from 'date-fns';
 import {
   createContext,
   ReactNode,
@@ -44,19 +45,34 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
   const [cyclesState, dispatch] = useReducer(cyclesReducer, {
     cycles: [],
     activeCycleId: null,
+  // eslint-disable-next-line consistent-return
+  }, () => {
+    const storedStateAsJSON = localStorage.getItem('@ignite-timer: cycles-state-1.0.0');
+
+    if (storedStateAsJSON) {
+      return JSON.parse(storedStateAsJSON);
+    }
   });
 
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+  const { cycles, activeCycleId } = cyclesState;
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(
+        new Date(),
+        new Date(activeCycle.startDate),
+      );
+    }
+    return 0;
+  });
 
   useEffect(() => {
     const stateJSON = JSON.stringify(cyclesState);
 
     localStorage.setItem('@ignite-timer: cycles-state-1.0.0', stateJSON);
   }, [cyclesState]);
-
-  const { cycles, activeCycleId } = cyclesState;
-
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
   function createNewCycle(data: CreateCycleData) {
     const id = String(new Date().getTime());
