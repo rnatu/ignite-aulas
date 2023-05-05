154ed9131ae7fe7067bf5c8544bbcbb9a59f21a0
Renato Xavier <rnatu91@gmail.com>
2023-05-05 17:57:52 -0300

    feat: add phosphor-react lib for icons

---
+++ reactjs-pt2/03-dt-money/src/components/Summary/index.tsx
@@ -0,0 +1,13 @@
+import { SummaryContainer } from "./styles";
+
+export function Summary() {
+  return (
+    <SummaryContainer>
+      <div>
+        <header>
+          <span>Entradas</span>
+        </header>
+      </div>
+    </SummaryContainer>
+  )
+}
