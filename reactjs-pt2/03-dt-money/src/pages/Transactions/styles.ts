ae7fafeff7b1ce4d6e4e8d0fd5749e199d0a4ead
Renato Xavier <rnatu91@gmail.com>
2023-05-08 12:02:02 -0300

    feat: add transactions table

---
+++ reactjs-pt2/03-dt-money/src/pages/Transactions/styles.ts
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const TransactionsContainer = styled.main`
+  width: 100%;
+  max-width: 1120px;
+
+  margin: 4rem auto 0;
+  padding: 0 1.5rem;
+`;
+
+export const TransactionsTable = styled.table`
+  width: 100%;
+  //! talvez não precise
+  /* border-collapse: separate; */
+  border-spacing: 0 0.5rem;
+
+  //! margin para barra de busca
+  /* margin-top: 1.5rem; */
+
+  td {
+    padding: 1.25rem 2rem;
+    background: ${(props) => props.theme["gray-700"]};
+
+    //first td
+    &:first-child {
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+
+    //last td
+    &:last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+  }
+`;
+
+interface PriceHighLightProps {
+  variant: "income" | "outcome";
+}
+
+export const PriceHighLight = styled.span<PriceHighLightProps>`
+  color: ${(props) =>
+    props.variant === "income"
+      ? props.theme["green-300"]
+      : props.theme["red-300"]};
+`;
