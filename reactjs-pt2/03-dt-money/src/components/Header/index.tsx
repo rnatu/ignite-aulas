2a0b1dbf1e0030fb0559d54f0ed4a5c506d3f9e3
Renato Xavier <rnatu91@gmail.com>
2023-05-08 13:49:24 -0300

    feat: add radixUI dialog to use as a modal

--- reactjs-pt2/03-dt-money/src/components/Header/index.tsx
+++ reactjs-pt2/03-dt-money/src/components/Header/index.tsx
@@ -1,14 +1,33 @@
+import * as Dialog from '@radix-ui/react-dialog';
+
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImg from '../../assets/logo.svg'
 
 export function Header() {
   return (
-   <HeaderContainer>
-    <HeaderContent>
-      <img src={logoImg} alt="" />
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="" />
 
-      <NewTransactionButton>Nova transação</NewTransactionButton>
-    </HeaderContent>
-   </HeaderContainer>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+              <Dialog.Title>Nova transação</Dialog.Title>
+             
+             
+              <Dialog.Close>x</Dialog.Close>
+            </Dialog.Content>
+
+          </Dialog.Portal>
+
+        </Dialog.Root>
+      </HeaderContent>
+    </HeaderContainer>
   )
 }
