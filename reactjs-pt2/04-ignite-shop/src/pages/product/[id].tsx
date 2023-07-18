cf1d0381880d591ae3b1a9b5270c53bec2f6f172
Renato Xavier <rnatu91@gmail.com>
2023-07-18 14:29:38 -0300

    style(Product): add style on Product page

--- reactjs-pt2/04-ignite-shop/src/pages/product/[id].tsx
+++ reactjs-pt2/04-ignite-shop/src/pages/product/[id].tsx
@@ -1,9 +1,25 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product";
 import { useRouter } from "next/router"
 
 
 export default function Product() {
   const { query } = useRouter();
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+        
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, necessitatibus id quo reiciendis aspernatur expedita eos ipsa maiores, praesentium reprehenderit laudantium veniam. Possimus, corporis officiis repudiandae cupiditate distinctio laborum voluptatum?</p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
