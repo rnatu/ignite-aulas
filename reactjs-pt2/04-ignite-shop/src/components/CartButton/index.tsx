2de543e9d30a3080e40ad1962e386f76ccf4492f
Renato Xavier <rnatu91@gmail.com>
2023-07-29 14:52:28 -0300

    feat: add CartButton on header

---
+++ reactjs-pt2/04-ignite-shop/src/components/CartButton/index.tsx
@@ -0,0 +1,10 @@
+import { Handbag } from "@phosphor-icons/react";
+import { CartButtonContainer } from "./styles";
+
+export function CartButton() {
+  return (
+    <CartButtonContainer>
+      <Handbag weight="bold"/>
+    </CartButtonContainer>
+  )
+}
---
+++ reactjs-pt2/04-ignite-shop/src/components/CartButton/styles.ts
@@ -0,0 +1,25 @@
+import { styled } from "@stitches/react";
+
+export const CartButtonContainer = styled("button", {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  border: "none",
+  borderRadius: 6,
+  position: "relative",
+
+  background: "$gray800",
+  color: "$gray500",
+
+  width: "3rem",
+  height: "3rem",
+
+  "&:disabled": {
+    opacity: 0.6,
+    cursor: "not-allowed",
+  },
+
+  svg: {
+    fontSize: "1.5rem",
+  },
+});
