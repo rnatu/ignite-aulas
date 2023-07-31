9d7bf4be9b511850b1fe414897622a99f1684fa0
Renato Xavier <rnatu91@gmail.com>
2023-07-31 13:16:31 -0300

    style(Cart): add cart item style

--- reactjs-pt2/04-ignite-shop/src/components/Cart/index.tsx
+++ reactjs-pt2/04-ignite-shop/src/components/Cart/index.tsx
@@ -1,7 +1,8 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import { CartButton } from "../CartButton";
-import { CartClose, CartContent } from './styles';
+import { CartClose, CartContent, CartFinalization, CartProduct, CartProductDetails, CartProductImage } from './styles';
 import { X } from '@phosphor-icons/react'
+import Image from 'next/image';
 
 export function Cart() {
   return (
@@ -19,8 +20,25 @@ export function Cart() {
           <h2>Sacola de compras</h2>
 
           <section>
-            <p>Parece que seu carrinho está vazio : (</p>
+            {/* <p>Parece que seu carrinho está vazio : (</p> */}
+
+            <CartProduct>
+              <CartProductImage>
+                <Image width={100} height={93} alt="" src="https://s3-alpha-sig.figma.com/img/387d/13ce/de131bd1ccf9bbe6b2331e88d3df20cd?Expires=1691971200&Signature=WwV3XHLg2RqdNTzI-3p5PhqFduQ6gvnU5fagtNi5FHzjzUM2eJ8afzQ9C~RsQlEP0QpxCssAHJ9iemB3yi3~DWpkSGqCS8iU1SaG6~tE33i82xOcB3cdAB-A6izptNUW0xLgvuwOP~oFQ6VEwXSww83v0UEwKV2GrFM7-FgEiDQSji3BZemEwyDlHDZ1MPnJKQl2nP8Jrm2~zOzaiMphPj1oLw05YKkidVw4W0d4GWvWruWxNEqUux35O5onktt4nMhde-42mSn~t4RlgZ6kDxxBJuWs3ej0yMXdKWsF5x~5gmWXPjMGQ5-23OQh~up7f0OhK3rRg7KriO7hOYWhvA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" />
+              </CartProductImage>
+
+              <CartProductDetails>
+                <p>Product 1</p>
+                <strong>R$ 50.00</strong>
+                <button>Remover</button>
+              </CartProductDetails>
+            </CartProduct>
+
           </section>
+
+          <CartFinalization>
+            <button>Finalizar Compra</button>
+          </CartFinalization>
         </CartContent>
       </Dialog.Portal>
     </Dialog.Root>
--- reactjs-pt2/04-ignite-shop/src/components/Cart/styles.ts
+++ reactjs-pt2/04-ignite-shop/src/components/Cart/styles.ts
@@ -1,8 +1,8 @@
 import { styled } from "../../styles/index";
-import * as Dialog from '@radix-ui/react-dialog'
+import * as Dialog from "@radix-ui/react-dialog";
 
 export const CartContent = styled(Dialog.Content, {
-  position: 'fixed',
+  position: "fixed",
   top: 0,
   right: 0,
   bottom: 0,
@@ -10,23 +10,98 @@ export const CartContent = styled(Dialog.Content, {
   background: "$gray800",
   padding: "3rem",
   paddingTop: "4.5rem",
-  boxShadow: "-4px 0px 30px rgba(0, 0, 0, 0.8)" ,
+  boxShadow: "-4px 0px 30px rgba(0, 0, 0, 0.8)",
   display: "flex",
   flexDirection: "column",
-  
+
   h2: {
     fontWeight: 700,
     fontSize: "$lg",
     color: "$gray100",
-    marginBottom: "2rem"
-  }
-})
+    marginBottom: "2rem",
+  },
+});
 
 export const CartClose = styled(Dialog.Close, {
-  background: 'none',
-  border: 'none',
+  background: "none",
+  border: "none",
   color: "$gray500",
   position: "absolute",
   top: "1.75rem",
   right: "1.75rem",
-})
+});
+
+export const CartProduct = styled("div", {
+  width: "100%",
+  display: "flex",
+  gap: "1.25rem",
+  alignItems: "center",
+  height: "5.8125rem",
+});
+
+export const CartProductImage = styled("div", {
+  width: "6.3125rem",
+  height: "5.8125rem",
+  background: "linear-gradient(180deg, #1ea483 0%, #7465d4 100%)",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  borderRadius: 8,
+
+  img: {
+    objectFit: "cover",
+  },
+});
+
+export const CartProductDetails = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  height: "100%",
+
+  p: {
+    color: "$gray300",
+    fontSize: "1.125rem",
+  },
+
+  strong: {
+    marginTop: 4,
+    fontSize: "$md",
+    fontWeight: "700",
+  },
+
+  button: {
+    marginTop: "auto",
+    width: "max-content",
+    background: "none",
+    border: "none",
+    color: "$green500",
+    fontSize: "1rem",
+    fontWeight: 700,
+  },
+});
+
+export const CartFinalization = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  marginTop: "auto",
+
+  button: {
+    width: "100%",
+    background: "$green500",
+    color: "$white",
+    fontSize: "$md",
+    height: "4.3125rem",
+    border: "none",
+    borderRadius: 8,
+    fontWeight: "700",
+
+    '&:disabled': {
+      opacity: 0.6,
+      cursor: "not-allowed",
+    },
+
+    '&:not(:disabled):hover': {
+      backgroundColor: '$green300'
+    }
+  },
+});
