3c56f7a286b23135c7d0dafce750691ea61b029b
Renato Xavier <rnatu91@gmail.com>
2023-08-01 14:23:18 -0300

    feat: create CartContext

--- reactjs-pt2/04-ignite-shop/src/pages/_app.tsx
+++ reactjs-pt2/04-ignite-shop/src/pages/_app.tsx
@@ -2,16 +2,19 @@ import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
 import { Container } from '../styles/pages/app';
 import { Header } from '../components/Header'
+import { CartContextProvider } from '@/contexts/CartContext';
 
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Container>
-      <Header />
+    <CartContextProvider>
+      <Container>
+        <Header />
 
-      <Component {...pageProps} />
-    </Container>
+        <Component {...pageProps} />
+      </Container>
+    </CartContextProvider>
   )
 }
