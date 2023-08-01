86e8dd7d2ab15077c7a6ccc65d8e5ecb590dbaac
Renato Xavier <rnatu91@gmail.com>
2023-08-01 14:28:51 -0300

    feat: create addToCart and checkIfItemAlreadyExists on context

--- reactjs-pt2/04-ignite-shop/src/contexts/CartContext.tsx
+++ reactjs-pt2/04-ignite-shop/src/contexts/CartContext.tsx
@@ -12,6 +12,8 @@ export interface IProduct {
 
 interface CartContextData {
   cartItems: IProduct[];
+  addToCart: (product: IProduct) => void;
+  checkIfItemAlreadyExists: (productId: string) => boolean;
 }
 
 interface CartContextProviderProps {
@@ -23,8 +25,16 @@ export const CartContext = createContext({} as CartContextData);
 export function CartContextProvider({ children }: CartContextProviderProps) {
   const [cartItems, setCartItems] = useState<IProduct[]>([]);
 
+  function addToCart(product: IProduct) {
+    setCartItems(oldState => [...oldState, product])
+  }
+
+  function checkIfItemAlreadyExists(productId: string) {
+    return cartItems.some(product => product.id === productId)
+  }
+
   return (
-    <CartContext.Provider value={{ cartItems }}>
+    <CartContext.Provider value={{ cartItems, addToCart, checkIfItemAlreadyExists }}>
       {children}
     </CartContext.Provider>
   )
