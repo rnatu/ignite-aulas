ef7eab5bb507e5618554f5a7535427a13c3995b3
Renato Xavier <rnatu91@gmail.com>
2023-08-09 11:25:53 -0300

    feat: add eslint on monorepo

--- reactjs-pt2/05-design-system/packages/tokens/src/index.ts
+++ reactjs-pt2/05-design-system/packages/tokens/src/index.ts
@@ -1,10 +1,10 @@
-export * from "./colors";
-export * from './colors';
+export * from './colors'
+export * from './colors'
 
-export * from "./space";
-export * from "./radii";
+export * from './space'
+export * from './radii'
 
-export * from "./fonts";
-export * from "./font-sizes";
-export * from "./font-weights";
-export * from "./line-heights";
+export * from './fonts'
+export * from './font-sizes'
+export * from './font-weights'
+export * from './line-heights'
