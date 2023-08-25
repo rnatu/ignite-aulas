583a9bca28d7332713852e3cf9f1acba7eb6d625
Renato Xavier <rnatu91@gmail.com>
2023-08-25 11:09:55 -0300

    feat: add Heading on storybook stories

--- reactjs-pt2/05-design-system/packages/docs/.storybook/preview.js
+++ reactjs-pt2/05-design-system/packages/docs/.storybook/preview.js
@@ -3,6 +3,9 @@ import { themes } from '@storybook/theming';
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
+    backgrounds: {
+      default: 'dark'
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
