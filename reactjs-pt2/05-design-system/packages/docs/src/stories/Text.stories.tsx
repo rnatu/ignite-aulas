21ec5d36f67e3cb93594f94d589c4826511549d3
Renato Xavier <rnatu91@gmail.com>
2023-08-25 10:39:03 -0300

    feat: add component text on design system

---
+++ reactjs-pt2/05-design-system/packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis vel repudiandae doloribus animi unde eius! Culpa voluptatum, eveniet tempora, unde, nobis veritatis consequatur porro architecto iure commodi animi ex quisquam.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text',
+    as: 'strong',
+  },
+}
