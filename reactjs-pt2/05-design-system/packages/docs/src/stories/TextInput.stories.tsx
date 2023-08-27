3ad13bc939ed87999ca630768f8fa864be38c9c7
Renato Xavier <rnatu91@gmail.com>
2023-08-27 16:32:54 -0300

    feat: add decoratos storybook on TextInput, using other stories

--- reactjs-pt2/05-design-system/packages/docs/src/stories/TextInput.stories.tsx
+++ reactjs-pt2/05-design-system/packages/docs/src/stories/TextInput.stories.tsx
@@ -1,10 +1,27 @@
 import type { StoryObj, Meta } from '@storybook/react'
-import { TextInput, TextInputProps } from '@ignite-ui/react'
+import { Box, Text, TextInput, TextInputProps } from '@ignite-ui/react'
 
 export default {
   title: 'Form/Text Input',
   component: TextInput,
   args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '$2',
+          }}
+        >
+          <Text size="sm">Email address</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
 } as Meta<TextInputProps>
 
 export const Primary: StoryObj<TextInputProps> = {
