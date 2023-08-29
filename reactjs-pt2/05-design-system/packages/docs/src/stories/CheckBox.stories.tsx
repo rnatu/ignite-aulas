1d86f70406a2266c5259a609e96e4d222984f753
Renato Xavier <rnatu91@gmail.com>
2023-08-29 20:44:05 -0300

    feat: add checkbox on react

---
+++ reactjs-pt2/05-design-system/packages/docs/src/stories/CheckBox.stories.tsx
@@ -0,0 +1,27 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, CheckBox, CheckBoxProps, Text } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/CheckBox',
+  component: CheckBox,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{
+            display: 'flex',
+            flexDirection: 'row',
+            gap: '$2',
+          }}
+        >
+          {Story()}
+          <Text size="sm">Accept terms of use</Text>
+        </Box>
+      )
+    },
+  ],
+} as Meta<CheckBoxProps>
+
+export const Primary: StoryObj<CheckBoxProps> = {}
