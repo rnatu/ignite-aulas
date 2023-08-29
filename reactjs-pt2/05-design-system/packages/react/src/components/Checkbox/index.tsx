0fd699d0d71610e03a03b083239e12da179de01c
Renato Xavier <rnatu91@gmail.com>
2023-08-29 13:07:58 -0300

    feat: add checkbox stories on storybook

---
+++ reactjs-pt2/05-design-system/packages/react/src/components/Checkbox/index.tsx
@@ -0,0 +1,15 @@
+import { Check } from 'phosphor-react'
+import { CheckBoxContainer, CheckBoxIndicator } from './styles'
+import { ComponentProps } from 'react'
+
+export type CheckBoxProps = ComponentProps<typeof CheckBoxContainer>
+
+export function CheckBox(props: CheckBoxProps) {
+  return (
+    <CheckBoxContainer {...props}>
+      <CheckBoxIndicator asChild>
+        <Check weight="bold" />
+      </CheckBoxIndicator>
+    </CheckBoxContainer>
+  )
+}
