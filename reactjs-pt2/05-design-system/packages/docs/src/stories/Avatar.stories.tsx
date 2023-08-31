64d0c28264885b987886f7e7b22ea32193e8005f
Renato Xavier <rnatu91@gmail.com>
2023-08-31 13:18:35 -0300

    fix: add control type text on argtypes at Avatar stories

--- reactjs-pt2/05-design-system/packages/docs/src/stories/Avatar.stories.tsx
+++ reactjs-pt2/05-design-system/packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/rnatu.png',
     alt: 'Renato Xavier',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
