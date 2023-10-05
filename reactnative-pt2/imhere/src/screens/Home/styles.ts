2d8c1dfb420417a99fe715a64de71d48e572cd31
Renato Xavier <rnatu91@gmail.com>
2023-10-05 12:20:02 -0300

    feat: add input on react native with style

--- reactnative-pt2/imhere/src/screens/Home/styles.ts
+++ reactnative-pt2/imhere/src/screens/Home/styles.ts
@@ -3,17 +3,25 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#131916',
+    backgroundColor: "#131916",
     padding: 24,
   },
   eventName: {
-    color: '#FDFCFE',
+    color: "#FFF",
     fontSize: 24,
-    fontWeight: 'bold',
-    marginTop: 48
+    fontWeight: "bold",
+    marginTop: 48,
   },
-  eventDate:  {
-    color: '#6B6B6B',
+  eventDate: {
+    color: "#6B6B6B",
     fontSize: 16,
-  }
-})
+  },
+  input: {
+    backgroundColor: '#1F1E25',
+    height: 56,
+    borderRadius: 5,
+    color: '#FFF',
+    padding: 16,
+    fontSize: 16,
+  },
+});
