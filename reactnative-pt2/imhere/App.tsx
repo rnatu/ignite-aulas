2ab55c9f41b4d35dd2745127bd06e2bdd0cf48b3
Renato Xavier <rnatu91@gmail.com>
2023-10-05 11:58:49 -0300

    feat(App): add styles with StyleSheet.create

--- reactnative-pt2/imhere/App.tsx
+++ reactnative-pt2/imhere/App.tsx
@@ -1,29 +1,28 @@
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View style={
-      {
-        backgroundColor: '#131916',
-        flex: 1,
-        padding: 24,
-
-      }
-    }>
-      <Text style={
-        {
-          color: '#FDFCFE',
-          fontSize: 24,
-          fontWeight: 'bold',
-          marginTop: 48
-        }
-      }>Nome do evento</Text>
-      <Text style={
-        {
-          color: '#6B6B6B',
-          fontSize: 16,
-        }
-      }>Sexta, 4 de Novembro de 2022.</Text>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#131916',
+    padding: 24,
+  },
+  eventName: {
+    color: '#FDFCFE',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48
+  },
+  eventDate:  {
+    color: '#6B6B6B',
+    fontSize: 16,
+  }
+})
