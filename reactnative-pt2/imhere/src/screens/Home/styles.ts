3607b6eead301a9c93938919f4f5b02e7754430e
Renato Xavier <rnatu91@gmail.com>
2023-10-06 10:10:25 -0300

    feat(Home): add view with style form do change flex direction

--- reactnative-pt2/imhere/src/screens/Home/styles.ts
+++ reactnative-pt2/imhere/src/screens/Home/styles.ts
@@ -16,13 +16,21 @@ export const styles = StyleSheet.create({
     color: "#6B6B6B",
     fontSize: 16,
   },
+  form: {
+    width: "100%",
+    flexDirection: "row",
+    marginTop: 36,
+    marginBottom: 42,
+  },
   input: {
+    flex: 1,
     backgroundColor: "#1F1E25",
     height: 56,
     borderRadius: 5,
     color: "#FFF",
     padding: 16,
     fontSize: 16,
+    marginRight: 12,
   },
   button: {
     width: 56,
